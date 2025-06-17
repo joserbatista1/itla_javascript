42ef0f5a7272f3c38e371845c18c9c6227a3daf9
joserbatista1 <149434440+joserbatista1@users.noreply.github.com>
2025-06-16 20:02:40 -0400

    Agregar boton de eliminar reserva html

--- client/src/app/components/ReservasList.jsx
+++ client/src/app/components/ReservasList.jsx
@@ -4,14 +4,28 @@ export default function ReservasList({ reservas, restaurante, hora }) {
       <h2 className="text-xl font-semibold mb-2 ">
         Reservas en {restaurante} a las {hora}
       </h2>
-      <ul  className="bg-white p-4 rounded shadow scroll-mt-20 space-y-2">
-        {reservas.length === 0 && <li>No hay reservas</li>}
-        {reservas.map(r => (
-          <li key={r.id} className="border-b py-1">
-            {r.nombre} - {r.cantidad} personas
-            <button type="button">Eliminar reserva</button></li>
-        ))}
-      </ul>
+    <ul className="bg-white p-4 rounded shadow scroll-mt-20 space-y-2">
+  {reservas.length === 0 && (
+    <li className="text-gray-500 italic">No hay reservas</li>
+  )}
+
+  {reservas.map((r) => (
+    <li
+      key={r.id}
+      className="border-b py-1 flex justify-between items-center"
+    >
+      <span>{r.nombre} - {r.cantidad} personas</span>
+      <button
+        type="button"
+        onClick={() => handleEliminar(r.id)}
+        className="text-sm text-red-600 hover:underline"
+      >
+        Eliminar reserva
+      </button>
+    </li>
+  ))}
+</ul>
+
     </div>
   );
 } 
